8ed85708e8db14cc87483981dc60049a29515c15
Narongsak Khotsakhoung <narongsak.k@arise.tech>
2025-01-17 09:06:54 +0700

    update script

--- What is k6/demo.js
+++ What is k6/demo.js
@@ -1,6 +1,7 @@
 import http from "k6/http";
-import { check } from "k6";
+import { check, sleep } from "k6";
 
 export default function() {
-    // console.log("Hello K6")
+  console.log("Hello K6");
+  sleep(1);
 }
